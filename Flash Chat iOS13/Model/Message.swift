8d650d7bdee6f81851bf031f6f389398cb9644cb
Luke Switzer <luke@rootdowndigital.com>
2022-06-30 09:57:51 -0600

    implement chat tableView methods

---
+++ Flash Chat iOS13/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Messages.swift
+//  Flash Chat iOS13
+//
+//  Created by Luke on 6/30/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
